311be20635347963618dce5d6c8ef99858e613f3
Ajay Kumar <ajayjnvkaimur@gmail.com>
2023-08-23 17:51:02 +0530

    'initial'

--- BoxStore/settings.py
+++ BoxStore/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6ip64v6!2r$n9zex)3=+7l1lh_2n(7vrvetwo(19##9zr20gre'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
